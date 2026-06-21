feac0ab03d4297af1834f5cc69f6ff1380b75de1
caturherupklh-cloud <caturherup.klh@gmail.com>
2026-06-22 06:23:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,38 +17,44 @@ const io = new Server(server, {
 const players = {}; 
 
 io.on('connection', (socket) => {
-  console.log('Pemain terhubung, menunggu pilihan karakter:', socket.id);
+  console.log('Koneksi baru masuk (belum login):', socket.id);
 
-  // BARU: Server mendengarkan pendaftaran (joinGame) dari layar pemilihan
-  socket.on('joinGame', (avatarType) => {
-      players[socket.id] = { 
-          x: 100, 
-          y: 100, 
-          id: socket.id,
-          direction: 'down',
-          isMoving: false,
-          avatar: avatarType // Menyimpan pilihan: 'boy' atau 'girl'
-      };
-      
-      socket.emit('currentPlayers', players);
-      socket.broadcast.emit('newPlayer', players[socket.id]);
+  // KODE BARU: Server menunggu event 'joinGame' dari frontend (membawa paket nama dan avatar)
+  socket.on('joinGame', (data) => {
+    // Daftarkan pemain BARU SAJA SETELAH mereka menekan tombol masuk
+    players[socket.id] = { 
+        x: 100, 
+        y: 100, 
+        id: socket.id,
+        direction: 'down',
+        isMoving: false,
+        avatar: data.avatar,    // Menyimpan avatar ('boy' atau 'girl')
+        playerName: data.name   // Menyimpan nama ketikan pemain
+    };
+    
+    socket.emit('currentPlayers', players);
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+    console.log(`Pemain ${socket.id} resmi masuk sebagai ${data.name} (${data.avatar})`);
   });
-
+  
   socket.on('playerMovement', (movementData) => {
-      // ... (biarkan isi movement tetap sama persis seperti sebelumnya)
-      players[socket.id].x = movementData.x;
-      players[socket.id].y = movementData.y;
-      players[socket.id].direction = movementData.direction; 
-      players[socket.id].isMoving = movementData.isMoving; 
+    // Pastikan pemain sudah terdaftar sebelum memproses pergerakan
+    if (players[socket.id]) {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        players[socket.id].direction = movementData.direction; 
+        players[socket.id].isMoving = movementData.isMoving; 
 
-      socket.broadcast.emit('playerMoved', players[socket.id]);
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    }
   });
 
   socket.on('disconnect', () => {
-      // ... (biarkan isi disconnect tetap sama persis)
-      console.log('Pemain keluar:', socket.id);
-      delete players[socket.id];
-      io.emit('playerDisconnected', socket.id);
+    console.log('Pemain terputus:', socket.id);
+    if (players[socket.id]) {
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    }
   });
 });
 
