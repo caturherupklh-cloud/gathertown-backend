947128f390ab3ddb9615ad2aba587321114303f0
caturherupklh-cloud <caturherup.klh@gmail.com>
2026-07-05 07:50:45 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ sudo apt-get install -y docker.io docker-compose git
 
 # 3. Kloning repositori GitHub kamu (Ganti URL dengan link repo-mu)
 # Pastikan repo bersifat publik agar tidak perlu password, atau gunakan Personal Access Token
-git clone https://github.com/username-kamu/mandatbumi-server.git
-cd mandatbumi-server
+git clone https://github.com/caturherupklh-cloud/gathertown-backend
+cd gathertown-backend
 
 # 4. Bangun dan nyalakan peladen di latar belakang
 sudo docker-compose up -d --build
